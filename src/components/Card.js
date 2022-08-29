ec0d4fefb13ac909b21f8a7d8d9580214c0297f1
Olga Bavykina <ozabotkina2@yandex.ru>
2022-08-29 16:47:48 +0300

    1st review

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,31 +1,23 @@
-import React from "react";
 
-class Card extends React.Component {
-constructor (props){
-  super(props);
-  this.handleClick = this.handleClick.bind(this);
-} 
-
-
-handleClick = () => {this.props.onCardClick(this.props.card)};
-
-
-render() {
-    return(
-        <div className="element" id={this.props.id}
-         onClick={this.handleClick}
-         >
-        <button className="element__trash interactive" type="button"></button>
-        <div className="element__image" style={{ backgroundImage: `url(${this.props.link})`}}></div>
-        <div className="element__words">
-          <h2 className="element__title">{this.props.name}</h2>
-          <div className="element__like-info">
-          <button className="element__like interactive" type="button"></button>
-          <p className="element__like-number">{this.props.likes.length}</p>
-          </div>
-          </div>
-       </div>   
-        );} 
-}
+function Card (props) {
+  
+  const handleClick = () => {props.onCardClick(props.card)};
+  
+      return(
+          <div className="element" id={props.id}
+           onClick={handleClick.bind(this)}
+           >
+          <button className="element__trash interactive" type="button"/>
+          <div className="element__image" style={{ backgroundImage: `url(${props.link})`}}/>
+          <div className="element__words">
+            <h2 className="element__title">{props.name}</h2>
+            <div className="element__like-info">
+            <button className="element__like interactive" type="button"/>
+            <p className="element__like-number">{props.likes.length}</p>
+            </div>
+            </div>
+         </div>   
+          );
+  }
 
 export default Card;
